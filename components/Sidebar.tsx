b23d1779df13c83d250792e95d3494acaa9e0929
Emily Walker <ewalker415@gmail.com>
2024-07-03 11:17:03 -0700

    added Sidebar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import '@/styles/Sidebar.css';
+
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+    <div className='logo text-9xl'>HAIR </div>
+    <div className='logo text-9xl ml-8'>TALK</div>
+    </div>
+  )
+}
+export default Sidebar
