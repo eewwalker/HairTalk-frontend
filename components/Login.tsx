b2e5fc7a8cf4a16948c78918b7694084f3a4ae9b
Emily Walker <ewalker415@gmail.com>
2024-07-03 11:19:12 -0700

    login component

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,8 +1,27 @@
 "use client";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { Scissors } from "lucide-react";
+import OverlayComponent from '@/components/OverlayComponent';
 
 
-export function Login(){
+export default function Login() {
+  const [showOverlay, setShowOverlay] = useState(false);
+
+  function loginRegister() {
+    setShowOverlay(true);
+  }
+
+  function handleClose() {
+    setShowOverlay(false);
+  }
   return (
-    <div>Login</div>
-  )
+    <div>
+      <Button variant='outline' size='icon' className="bg-inherit rounded-full hover:bg-[#c1f5fe]" onClick={loginRegister}>
+        <Scissors className='text-[#ff99dd]' />
+      </Button>
+      {showOverlay && <OverlayComponent onClose={handleClose}/>}
+
+    </div>
+  );
 }
