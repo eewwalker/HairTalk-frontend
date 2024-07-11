b6cbd14113a1901827f474aa0e2d289efefa300f
Emily Walker <ewalker415@gmail.com>
2024-07-11 14:36:13 -0700

    add Overlay Component

--- components/OverlayComponent.tsx
+++ components/OverlayComponent.tsx
@@ -9,7 +9,7 @@ const OverlayComponent: React.FC<Overlay> = ({onClose }) => {
     return(
         <div className="overlay p-7 flex flex-col">
             <Button onClick={onClose} variant='outline' size='icon' className="bg-inherit rounded-full hover:bg-[#c1f5fe]">X</Button>
-            <Link href='/signup' className="text-7xl m-3">SIGNUP</Link>
+            <Link href='/register' className="text-7xl m-3">SIGNUP</Link>
             <Link href='/login'className="text-7xl m-3">LOGIN</Link>
             <Link href='/logout'className="text-7xl m-3">LOGOUT</Link>
         </div>
