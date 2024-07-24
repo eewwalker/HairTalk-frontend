9b33425cd807e015fe7268a2292e4ae52c59d60e
Emily Walker <ewalker415@gmail.com>
2024-07-24 15:46:38 -0700

    add nextauth provider and types

--- types.ts
+++ types.ts
@@ -1,14 +1,19 @@
+import NextAuth, {DefaultSession} from "next-auth";
+import {JWT} from "next-auth/jwt";
+
 export interface User {
   id: number;
   username: string;
   password: string;
   location: string | null;
+  access_token?: string;
 }
 export interface Question {
   id: number;
-  userId: number;
+  user_id: number;
   content: string;
   created_at: Date;
+  answers: number;
 
 }
 export interface Overlay {
@@ -23,10 +28,31 @@ export interface Token {
   access_token: string;
   userId: number;
 }
-
-export interface AuthContextType {
-  user: User | null;
-  login: (username:string, password:string) => Promise<void>;
-  logout: () => void;
-  loading: true | false;
+//Extend the NextAuth Session and User types
+declare module 'next-auth' {
+  interface Session {
+    accessToken?: string;
+    user: {
+      id: number;
+      username: string;
+      location: string | null;
+    } & DefaultSession['user'];
+  }
+  interface User {
+    id: number;
+    username: string;
+    password: string;
+    location: string | null;
+    access_token?: string;
+  }
 }
+//Extend NextAuth JWT interface
+declare module 'next-auth/jwt' {
+  interface JWT {
+    accessToken: string;
+    userId?: number | string;
+    username?: string;
+    location?: string | null;
+  }
+}
+
