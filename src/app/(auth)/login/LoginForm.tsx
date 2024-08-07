69d73b6e723c772c4a4fa162aff52cb695358141
Emily Walker <ewalker415@gmail.com>
2024-08-07 15:40:17 -0700

    incorporate nextAuth for logIn and finish register user logic

---
+++ src/app/(auth)/login/LoginForm.tsx
@@ -0,0 +1,93 @@
+'use client';
+import Link from "next/link"
+
+import { Button } from "@/src/components/ui/button"
+import { Input } from "@/src/components/ui/input"
+import { Label } from "@/src/components/ui/label"
+import { useForm } from "react-hook-form";
+import { loginSchema, LoginSchema } from "@/src/lib/schemas/loginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+// import { useToast } from "@/components/ui/toast";
+
+
+const LoginForm = () => {
+  const router = useRouter();
+  // const toast = useToast()
+  const {register, handleSubmit, formState: {errors, isValid, isSubmitting}} = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    mode: 'onTouched'
+  });
+
+  const onSubmit = async (data:LoginSchema) => {
+    console.log('data', data)
+    const result = await signIn('credentials', {
+      username: data.username,
+      password: data.password,
+      redirect: false
+    });
+    console.log('result', result)
+    if (result && !result.error) {
+      router.push('/');
+    }else {
+      // toast({
+      //   description: resp.error as string
+      // });
+      console.log(result?.error)
+    }
+  }
+
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-1 xl:min-h-[800px]">
+    <div className="flex items-center justify-center py-12">
+      <div className="mx-auto grid w-[350px] gap-6">
+        <div className="grid gap-2 text-center">
+          <h1 className="text-5xl font-bold text-[#66ffec]">login</h1>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid gap-4">
+          <div className="grid gap-2 text-[#66ffec]">
+            <Label htmlFor="username">username</Label>
+            <Input
+              className="text-[#0c6999]"
+              required
+              autoComplete="username"
+              {...register('username')}
+              />
+              {errors.username?.message && <p>{errors.username?.message}</p>}
+          </div>
+          <div className="grid gap-2 text-[#66ffec]">
+            <div className="flex items-center">
+              <Label htmlFor="password">password</Label>
+            </div>
+            <Input
+              className="text-[#0c6999]"
+              id="password"
+              type="password"
+              required
+              autoComplete="current-password"
+              {...register('password')}
+              />
+              {errors.password?.message && <p>{errors.password?.message}</p>}
+          </div>
+          <Button
+            disabled={!isValid || isSubmitting}
+            type="submit"
+            className="w-full hover:bg-[#2584b3] hover:text-[#66ffec] rounded-full bg-[#c1f5fe] text-[#0c6999]">
+            login
+          </Button>
+        </div>
+        </form>
+        <div className="mt-4 text-center text-sm text-[#66ffec]">
+          don&apos;t have an account?{" "}
+          <Link href="/register" className="text-decoration-none">
+            register
+          </Link>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+export default LoginForm
---
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -0,0 +1,88 @@
+"use client";
+import Link from "next/link";
+
+import { Button } from "@/src/components/ui/button";
+import { Input } from "@/src/components/ui/input";
+import { Label } from "@/src/components/ui/label";
+import { useForm } from "react-hook-form";
+import { RegisterSchema, registerSchema } from "@/src/lib/schemas/registerSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createNewUser } from "@/src/lib/api";
+import { useRouter } from "next/navigation";
+
+const RegisterForm = () => {
+    const router = useRouter();
+    const { register, handleSubmit, formState: { errors } } = useForm<RegisterSchema>({
+        resolver: zodResolver(registerSchema),
+        mode: 'onTouched'
+    });
+
+    const onSubmit = async (data: RegisterSchema) => {
+        const {username, password, location} = data;
+        const resp = await createNewUser(username, password,location);
+        console.log('resp', resp)
+        router.push('/login');
+    };
+
+    return (
+        <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-1 xl:min-h-[800px]">
+            <div className="flex items-center justify-center py-12">
+                <div className="mx-auto grid w-[350px] gap-6">
+                    <div className="grid gap-2 text-center">
+                        <h1 className="text-5xl font-bold text-[#66ffec]">register</h1>
+                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="grid gap-4">
+                            <div className="grid gap-2 text-[#66ffec]">
+                                <Label htmlFor="username">username</Label>
+                                <Input
+                                    className="text-[#0c6999]"
+                                    autoComplete="username"
+                                    id="username"
+                                    type="text"
+                                    {...register('username')}
+                                />
+                                {errors.username?.message && <p>{errors.username?.message}</p>}
+                            </div>
+                            <div className="grid gap-2 text-[#66ffec]">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">password</Label>
+                                </div>
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    {...register('password')}
+                                    autoComplete="new-password"
+                                />
+                                {errors.password?.message && <p>{errors.password?.message}</p>}
+                            </div>
+                            <div className="grid gap-2 text-[#66ffec]">
+                                <Label htmlFor="location">location</Label>
+                                <Input
+                                    className="text-[#0c6999]"
+                                    autoComplete="location"
+                                    id="location"
+                                    type="text"
+                                    {...register('location')}
+                                />
+                                {errors.location?.message && <p>{errors.location?.message}</p>}
+                            </div>
+                            <Button
+                                type="submit"
+                                className="w-full hover:bg-[#2584b3] hover:text-[#66ffec] rounded-full bg-[#c1f5fe] text-[#0c6999]">
+                                sign up
+                            </Button>
+                        </div>
+                    </form>
+                    <div className="mt-4 text-center text-sm text-[#66ffec]">
+                        already have an account?{" "}
+                        <Link href="/login" className="text-decoration-none">
+                            login
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default RegisterForm;
