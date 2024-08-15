82dbe1205694547e0b177c01577df886df49ef2f
Emily Walker <ewalker415@gmail.com>
2024-08-15 14:47:04 -0700

    removed authComponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 
-import AuthComponent from '../components/AuthComponent';
 import Sidebar from '../components/Sidebar';
 import Questions from '../components/Questions';
 
@@ -11,17 +10,14 @@ export default function Home() {
 
   return (
     <>
-        <div className="flex flex-col min-h-screen">
-          <div className="flex flex-1">
-            <Sidebar />
-            <div className="flex-1 flex flex-col items-center justify-center">
-              <Questions />
-            </div>
-          </div>
-          <div className="fixed top-10 right-5">
-            <AuthComponent />
+      <div className="flex flex-col min-h-screen">
+        <div className="flex flex-1">
+          <Sidebar />
+          <div className="flex-1 flex flex-col items-center justify-center">
+            <Questions />
           </div>
         </div>
+      </div>
     </>
   );
 }
