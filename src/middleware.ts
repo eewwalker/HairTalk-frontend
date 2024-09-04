7e3e73227608e0178529d4f92b97efb53f51cc4a
Emily Walker <ewalker415@gmail.com>
2024-09-04 16:22:01 -0700

    add middleware to authorize routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,10 @@
-export {default} from 'next-auth/middleware';
 
-export const config = {matcher: ['/dashboard(.*)']}
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+})
+
+export const config = { matcher: ["/questions/ask"] }
