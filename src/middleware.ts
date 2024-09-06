da1e413ac3da560df7e15cf1144a7dbe7eeeccf9
Emily Walker <ewalker415@gmail.com>
2024-09-05 18:52:53 -0700

    add comments for middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,12 @@
-
+//withAuth function from middleware module
 import { withAuth } from "next-auth/middleware"
 
+//if unauthentication user tries to access protected route send to '/login'
 export default withAuth({
   pages: {
     signIn: "/login",
   },
 })
 
+//tells next which routes the middleware should run 
 export const config = { matcher: ["/questions/ask"] }
