3d0ba043351dd07397feb6213129c2e9d2bb48f2
Emily Walker <ewalker415@gmail.com>
2024-11-13 17:01:28 -0800

    update tag to display if there are tags associated with quesiton

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,15 +1,17 @@
-"use client";
-import {Question as QuestionType} from "@/types";
-import {useSession} from "next-auth/react"
+// "use client";
+import { Question as QuestionType } from "@/types";
+// import {useSession} from "next-auth/react"
 import Link from "next/link";
 
 type Props = {
-    question: QuestionType
-}
+    question: QuestionType;
+};
 
 
-export default function Question({question}: Props) {
-    const {data: session} = useSession();
+export default function Question({ question }: Props) {
+    console.log('question', question)
+    console.log(question.tags, 'tagsss')
+    // const {data: session} = useSession();
     const dateString = question.created_at.toString();
     return (
         <div className="border p-4 rounded-md shadow-sm mb-4 w-full max-w-lg sm:max-w-xl md:max-w-2xl">
@@ -17,7 +19,7 @@ export default function Question({question}: Props) {
                 {dateString}
             </div>
             <div className="text-[#f5f1ef] text-lg mb-1">
-               <Link href={`/questions/${question.id}/${question.title}`}> {question.title}</Link>
+                <Link href={`/questions/${question.id}/${question.title}`}> {question.title}</Link>
             </div>
             <div className="text-[#f5f1ef] text-sm mb-1">
                 {question.content}
@@ -26,16 +28,16 @@ export default function Question({question}: Props) {
                 <span>0 votes</span>
                 <span>0 answers</span>
             </div>
-            <div className="flex space-x-2 mt-2 flex-wrap">
-                <span className="bg-gray-200 text-gray-700 px-2 py-1 rounded-md">tag1</span>
-                <span className="bg-gray-200 text-gray-700 px-2 py-1 rounded-md">tag2</span>
-                <span className="bg-gray-200 text-gray-700 px-2 py-1 rounded-md">tag3</span>
-            </div>
+            {question.tags.length > 0 &&
+                <div className="flex space-x-2 mt-2 flex-wrap">
+                    {question.tags.map((t, i) =>  <span key={i} className="bg-gray-200 text-gray-700 px-2 py-1 rounded-md">{t}</span>)}
+                </div>
+            }
             <div>
                 <div className="">
                     Something about the user that is posting the question
                 </div>
             </div>
         </div>
-    )
+    );
 }
